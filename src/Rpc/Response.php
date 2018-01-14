7bac0ec2d201c657fcd55b449ea8243c4332f4d1
lilin <794774870@qq.com>
2018-01-14 17:09:43 +0800

    http-message

--- src/Rpc/Response.php
+++ src/Rpc/Response.php
@@ -2,11 +2,8 @@
 
 namespace Swoft\Rpc\Server\Rpc;
 
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\StreamInterface;
-
 /**
- *
+ * the response of rpc
  *
  * @uses      Response
  * @version   2018年01月09日
@@ -14,134 +11,7 @@ use Psr\Http\Message\StreamInterface;
  * @copyright Copyright 2010-2016 swoft software
  * @license   PHP Version 7.x {@link http://www.php.net/license/3_0.txt}
  */
-class Response implements ResponseInterface
+class Response extends \Swoft\Http\Message\Base\Response
 {
-    /**
-     * @var array
-     */
-    private $attributes = [];
-
-    /**
-     * Retrieve attributes derived from the request.
-     * The request "attributes" may be used to allow injection of any
-     * parameters derived from the request: e.g., the results of path
-     * match operations; the results of decrypting cookies; the results of
-     * deserializing non-form-encoded message bodies; etc. Attributes
-     * will be application and request specific, and CAN be mutable.
-     *
-     * @return array Attributes derived from the request.
-     */
-    public function getAttributes()
-    {
-        return $this->attributes;
-    }
-
-    /**
-     * Retrieve a single derived request attribute.
-     * Retrieves a single derived request attribute as described in
-     * getAttributes(). If the attribute has not been previously set, returns
-     * the default value as provided.
-     * This method obviates the need for a hasAttribute() method, as it allows
-     * specifying a default value to return if the attribute is not found.
-     *
-     * @see getAttributes()
-     * @param string $name    The attribute name.
-     * @param mixed  $default Default value to return if the attribute does not exist.
-     * @return mixed
-     */
-    public function getAttribute($name, $default = null)
-    {
-        return array_key_exists($name, $this->attributes) ? $this->attributes[$name] : $default;
-    }
-
-    /**
-     * Return an instance with the specified derived request attribute.
-     * This method allows setting a single derived request attribute as
-     * described in getAttributes().
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the message, and MUST return an instance that has the
-     * updated attribute.
-     *
-     * @see getAttributes()
-     * @param string $name  The attribute name.
-     * @param mixed  $value The value of the attribute.
-     * @return static
-     */
-    public function withAttribute($name, $value)
-    {
-        $clone = clone $this;
-        $clone->attributes[$name] = $value;
-        return $clone;
-    }
-
-    public function getProtocolVersion()
-    {
-        // TODO: Implement getProtocolVersion() method.
-    }
-
-    public function withProtocolVersion($version)
-    {
-        // TODO: Implement withProtocolVersion() method.
-    }
-
-    public function getHeaders()
-    {
-        // TODO: Implement getHeaders() method.
-    }
-
-    public function hasHeader($name)
-    {
-        // TODO: Implement hasHeader() method.
-    }
-
-    public function getHeader($name)
-    {
-        // TODO: Implement getHeader() method.
-    }
-
-    public function getHeaderLine($name)
-    {
-        // TODO: Implement getHeaderLine() method.
-    }
-
-    public function withHeader($name, $value)
-    {
-        // TODO: Implement withHeader() method.
-    }
-
-    public function withAddedHeader($name, $value)
-    {
-        // TODO: Implement withAddedHeader() method.
-    }
-
-    public function withoutHeader($name)
-    {
-        // TODO: Implement withoutHeader() method.
-    }
-
-    public function getBody()
-    {
-        // TODO: Implement getBody() method.
-    }
-
-    public function withBody(StreamInterface $body)
-    {
-        // TODO: Implement withBody() method.
-    }
-
-    public function getStatusCode()
-    {
-        // TODO: Implement getStatusCode() method.
-    }
-
-    public function withStatus($code, $reasonPhrase = '')
-    {
-        // TODO: Implement withStatus() method.
-    }
-
-    public function getReasonPhrase()
-    {
-        // TODO: Implement getReasonPhrase() method.
-    }
 
 }
